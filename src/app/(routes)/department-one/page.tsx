0ff328cecb439e7f17b963d728f4ba1e7d35c089
Jose Carrasco <jscarrasco18@outlook.com>
2023-12-15 00:24:31 -0400

    estrucutura inicial

---
+++ src/app/(routes)/department-one/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const DepartmentOnePage = () => {
+  return <div>DepartmentOnePage</div>;
+};
+
+export default DepartmentOnePage;
---
+++ src/data/navbarOptions.ts
@@ -0,0 +1,11 @@
+"use client";
+export const NavbarOptions = [
+  {
+    ref: "/home",
+    text: "Home Page",
+  },
+  {
+    ref: "/department-one",
+    text: "department one",
+  },
+];
