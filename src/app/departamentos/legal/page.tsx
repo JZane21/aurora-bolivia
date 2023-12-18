00ba54975254defbc17b211087af13ff681f30ec
¨lizarazu22¨ <lizarazuaramayoignacio@gmail.com>
2023-12-18 07:19:39 -0400

    commit1

--- src/app/departamentos/legal/page.tsx
+++ src/app/departamentos/legal/page.tsx
@@ -1,8 +1,181 @@
+import React from 'react';
+import styles from './LegalPage.module.css';
+
 const LegalPage = () => {
-    return (
-        <>Página de Departamento Legal</>
-    );
+  return (
+    <div className={styles.pageContainer}>
+      <h2 className={styles.title}>Departamento Legal</h2>
+      
+      {/* Sección Quiénes Somos */}
+      <div className={styles.section}>
+        <h3 className={styles.title}>Quiénes Somos</h3>
+        <p className={styles.content}>
+        ¿Quiénes Somos?
+El Departamento Legal de Aurora es un equipo de profesionales jurídicos altamente calificados, especializados en el derecho tecnológico y corporativo. Nos dedicamos a proteger los intereses de Aurora, asegurando el cumplimiento normativo y gestionando eficazmente los riesgos legales. Nuestra experiencia abarca una amplia gama de áreas, incluyendo propiedad intelectual, contratos, cumplimiento de privacidad de datos y litigios. Trabajamos estrechamente con todos los departamentos para proporcionar asesoramiento legal estratégico y apoyar las iniciativas de negocio de la empresa.
+
+        </p>
+      </div>
+      
+      {/* Sección Misión */}
+      <div className={styles.section}>
+        <h3 className={styles.title}>Misión</h3>
+        <p className={styles.content}>
+        Misión
+Nuestra misión es garantizar que Aurora opere dentro de un marco legal sólido y seguro, proporcionando asesoramiento legal proactivo y soluciones jurídicas efectivas. Nos esforzamos por minimizar los riesgos legales y maximizar las oportunidades para la empresa, asegurando el cumplimiento de las leyes y regulaciones aplicables. Nos comprometemos a mantener a Aurora al frente de las cuestiones legales emergentes en el sector tecnológico, fortaleciendo la confianza y la integridad de la empresa.
+
+        </p>
+      </div>
+      
+      {/* Sección Visión */}
+      <div className={styles.section}>
+        <h3 className={styles.title}>Visión</h3>
+        <p className={styles.content}>
+        Visión
+La visión del Departamento Legal de Aurora es ser reconocidos como un equipo jurídico líder en el sector tecnológico, distinguido por nuestra capacidad para anticipar y navegar por el cambiante paisaje legal global. Aspiramos a ser un pilar fundamental en el crecimiento y éxito de Aurora, proporcionando orientación legal estratégica que respalde la innovación y el desarrollo empresarial. Nuestro objetivo es establecer un estándar de excelencia en la gestión legal, asegurando que Aurora sea sinónimo de integridad, responsabilidad y compromiso con la legalidad.
+
+        </p>
+      </div>
+      
+      {/* Sección Manual de Funciones */}
+      <div className={styles.section}>
+        <h3 className={styles.title}>Manual de Funciones</h3>
+        <pre className={styles.manualContent}>
+{` Manual de Funciones del Departamento Legal en Aurora
+
+Función General
+El Departamento Legal de Aurora es responsable de gestionar todos los aspectos legales de la empresa, incluyendo el cumplimiento normativo, la gestión de riesgos, la asesoría en contratos y la protección de la propiedad intelectual. Este departamento juega un papel crucial en apoyar las operaciones empresariales y en facilitar el crecimiento estratégico de la compañía.
+
+Responsabilidades Clave
+
+1. Asesoramiento Legal y Estratégico
+  - Proporcionar asesoramiento legal proactivo en todas las áreas de operación de la empresa.
+  - Participar en la planificación estratégica para identificar y abordar posibles implicaciones legales.
+
+2. Gestión de Contratos
+  - Redactar, revisar y negociar contratos con clientes, proveedores y socios.
+  - Asegurar que todos los contratos cumplan con las leyes y regulaciones aplicables.
+
+3. Cumplimiento Normativo
+  - Supervisar el cumplimiento de la empresa con todas las leyes y regulaciones pertinentes.
+  - Mantenerse actualizado sobre los cambios en la legislación que puedan afectar a la empresa.
+
+4. Gestión de la Propiedad Intelectual
+  - Proteger los activos de propiedad intelectual de la empresa, incluyendo patentes, marcas y derechos de autor.
+  - Gestionar litigios relacionados con la propiedad intelectual, si los hubiere.
+
+5. Resolución de Disputas y Litigios
+  - Manejar disputas legales y procedimientos judiciales.
+  - Coordinar con asesores legales externos cuando sea necesario.
+
+6. Formación y Concienciación
+  - Proporcionar formación y concienciación sobre cuestiones legales a los empleados.
+  - Asegurar que el personal esté informado sobre las prácticas legales pertinentes a sus roles.
+
+7. Gestión de Riesgos
+  - Identificar y evaluar los riesgos legales que enfrenta la empresa.
+  - Desarrollar estrategias para mitigar los riesgos identificados.
+
+Competencias y Habilidades
+
+- Conocimientos Jurídicos Profundos: Expertise en áreas de ley relevantes para el sector tecnológico y empresarial.
+- Habilidades de Negociación y Comunicación: Capacidad para negociar y comunicar efectivamente términos legales complejos.
+- Análisis y Resolución de Problemas: Aptitud para analizar situaciones legales y desarrollar soluciones estratégicas.
+- Orientación al Detalle: Precisión y atención al detalle en la redacción y revisión de documentos legales.
+
+Evaluación del Desempeño
+El rendimiento del Departamento Legal será evaluado anualmente, basándose en su capacidad para gestionar eficazmente los asuntos legales de la empresa, mitigar riesgos y apoyar los objetivos empresariales de Aurora.
+`}
+        </pre>
+      </div>
+      
+      {/* Sección Manual de Actividades */}
+      <div className={styles.section}>
+        <h3 className={styles.title}>Actividades</h3>
+        <pre className={styles.manualContent}>
+{` Manual de Actividades para el Departamento Legal de Aurora
+
+Actividades Diarias
+1. Revisión de Comunicaciones Legales
+  - Revisar y responder a comunicaciones legales internas y externas.
+  - Mantenerse actualizado sobre consultas legales de otros departamentos.
+
+2. Gestión de Contratos
+  - Revisar y redactar contratos, acuerdos y otros documentos legales.
+  - Realizar seguimiento de los contratos en curso y asegurar su cumplimiento.
+
+3. Monitoreo de Cumplimiento
+  - Verificar el cumplimiento diario de las normativas legales y reglamentarias en las operaciones de la empresa.
+
+Actividades Semanales
+1. Reuniones con Departamentos
+  - Mantener reuniones regulares con otros departamentos para discutir asuntos legales y ofrecer asesoría.
+  - Coordinar con el equipo de gestión para revisar y mitigar riesgos legales en proyectos y decisiones empresariales.
+
+2. Revisión de Políticas y Procedimientos
+  - Evaluar y actualizar las políticas y procedimientos internos para garantizar el cumplimiento normativo.
+
+3. Formación y Concienciación
+  - Organizar sesiones de formación y concienciación legal para empleados.
+
+Actividades Mensuales
+1. Informe Legal Mensual
+  - Preparar y presentar un informe mensual sobre el estado legal de la empresa.
+  - Revisar y actualizar el registro de riesgos legales.
+
+2. Revisión de Tendencias Legales
+  - Investigar y reportar sobre tendencias legales y cambios regulatorios que puedan impactar a la empresa.
+
+3. Gestión de la Propiedad Intelectual
+  - Revisar y gestionar asuntos relacionados con la propiedad intelectual.
+
+Actividades Trimestrales
+1. Auditorías Legales Internas
+  - Realizar o coordinar auditorías internas para asegurar el cumplimiento legal en todas las áreas de la empresa.
+
+2. Evaluación de Riesgos Legales
+  - Realizar una evaluación trimestral de los riesgos legales y actualizar la estrategia de mitigación.
+
+3. Revisión de Estrategias Legales
+  - Evaluar y actualizar las estrategias legales de la empresa en función del entorno empresarial y legal cambiante.
+
+Actividades Anuales
+1. Planificación Estratégica Legal
+  - Participar en la planificación estratégica anual, aportando perspectivas legales.
+  - Revisar y establecer objetivos anuales para el departamento.
+
+2. Informes a la Junta Directiva
+  - Preparar y presentar informes anuales a la junta directiva sobre asuntos legales clave.
+
+3. Revisión de Cumplimiento Anual
+  - Realizar una revisión completa del cumplimiento normativo de la empresa.
+
+4. Formación y Desarrollo Profesional
+  - Participar en conferencias y formaciones legales para mantenerse actualizado en el derecho corporativo y tecnológico.
+
+
+`}
+        </pre>
+      </div>
+
+      {/* Sección Workflow */}
+      <div className={styles.workflowSection}>
+        <div>
+          <h3 className={styles.title}>Workflow</h3>
+          <p className={styles.content}>Descripción del flujo de trabajo del Departamento Legal.</p>
+        </div>
+        
+        <div>
+          <h3 className={styles.title}>Cómo Interactúa la Empresa</h3>
+          <p className={styles.content}>Explicación de la interacción con la empresa...</p>
+        </div>
+        
+        <div>
+          <h3 className={styles.title}>Modelo de Trabajo</h3>
+          <p className={styles.content}>Explicación del modelo de trabajo...</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LegalPage;
-
--- src/app/quienes-somos/page.tsx
+++ src/app/quienes-somos/page.tsx
@@ -1,6 +1,25 @@
+// En el archivo QuienesSomosPage.js
+
+import './QuienesSomosPage.css';
+
 const QuienesSomosPage = () => {
     return (
-        <>Página de ¿Quiénes Somos?</>
+        <div className="quienes-somos-container">
+            <section>
+                <h1>¿Quiénes Somos?</h1>
+                <p>Somos un colectivo de emprendedores apasionados por la tecnología, dedicados a impulsar la innovación en el sector. Nuestro equipo, compuesto por expertos en Ingeniería de Sistemas, se especializa en diversas áreas clave, lo que nos permite ofrecer soluciones integrales y vanguardistas. El proyecto que iniciamos es una piedra en la gran pirámide que deseamos construir, debido a que la visión que tenemos no está centrada solamente en el éxito económico, está inclinada hacia la superación individual y colectiva.</p>
+            </section>
+
+            <section>
+                <h2>Misión</h2>
+                <p>Nuestra misión es desentrañar y simplificar los desafíos tecnológicos, ofreciendo soluciones eficaces y personalizadas. Nos enfocamos en minimizar problemas y maximizar eficiencia, asegurando que cada proyecto se realice con agilidad y precisión. Priorizamos la satisfacción total del cliente, garantizando que cada solución no solo cumpla, sino supere sus expectativas.</p>
+            </section>
+
+            <section>
+                <h2>Visión</h2>
+                <p>En Aurora, nos proyectamos como una empresa líder y referente en el ámbito tecnológico a nivel nacional. Nuestro objetivo es destacarnos por nuestra capacidad de innovar y por la excelencia en la prestación de servicios de comunicación y desarrollo de nuevas tecnologías. Nos comprometemos a ser un pilar en el avance tecnológico, adaptándose y evolucionando constantemente para satisfacer las demandas dinámicas de nuestros clientes.</p>
+            </section>
+        </div>
     );
 };
 
