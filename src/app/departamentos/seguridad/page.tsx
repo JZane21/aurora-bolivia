358cb70d1dcaf95c8754c7291c3e98dce9977dbe
Richard Rojas <richard.rojas@nologin.com.bo>
2023-12-18 03:36:57 -0400

    add

--- src/app/departamentos/seguridad/page.tsx
+++ src/app/departamentos/seguridad/page.tsx
@@ -45,8 +45,73 @@ const SeguridadInformaticaPage = () => {
           </label>
         </div>
       </div>
+
       <div> Actividades</div>
-      <div> </div>
+
+      <div>
+        <div className="text-white font-bold text-[50px] text-center">At tempor commodo</div>
+
+        <div className="text-white font-normal text-[32px]">Odio tempor orci dapibus ultrices</div>
+        <div className="text-white grid grid-cols-4 gap-1 w-full content-start">
+            <div className="bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] p-4 m-2">
+                <div className="text-[#889CE7]">Quam pellentesque nec nam aliquam</div>
+                <div>
+                Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Amet venenatis urna cursus eget nunc scelerisque. Diam vulputate ut pharetra sit amet aliquam id diam maecenas. Velit ut tortor pretium viverra suspendisse potenti nullam.
+                </div>
+            </div>
+            <div className="bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] p-4 m-2">
+                <div className="text-[#889CE7]">Laoreet id donec ultrices tincidunt arcu non sodales</div>
+                <div>
+                Tristique et egestas quis ipsum suspendisse ultrices gravida. Pharetra vel turpis nunc eget lorem. Fames ac turpis egestas integer eget aliquet nibh praesent tristique.
+                </div>
+            </div>
+            <div className="bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] p-4 m-2">
+                <div className="text-[#889CE7]">Ultrices neque ornare</div>
+                <div>
+                Amet risus nullam eget felis. Lectus quam id leo in vitae turpis massa. Rhoncus mattis rhoncus urna neque viverra justo nec. Id velit ut tortor pretium viverra suspendisse potenti nullam ac.
+                </div>
+            </div>
+            <div className="bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] p-4 m-2">
+                <div className="text-[#889CE7]">Eros donec ac odio</div>
+                <div>
+                Turpis tincidunt id aliquet risus feugiat in ante. Duis at tellus at urna condimentum mattis pellentesque id. Amet aliquam id diam maecenas ultricies mi. Consequat ac felis donec et odio.
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="text-white font-bold text-[50px] text-center">Lorem ipsum dolor sit amet</div>
+        
+        <div className="text-white font-normal text-[32px]">Odio tempor orci dapibus ultrices</div>
+        <div className="text-white grid grid-cols-4 gap-4 w-full content-start text-center">
+            <div className="bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] p-4 m-4">
+                <div className="text-[18px] text-[#889CE7]">Auctor</div>
+                <div className="text-[13px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Nisl purus in mollis nunc sed id. Lobortis feugiat vivamus at augue eget. Diam phasellus vestibulum lorem sed risus ultricies. Pellentesque id nibh tortor id aliquet lectus. Erat imperdiet sed euismod nisi porta lorem mollis aliquam. Ullamcorper dignissim cras tincidunt lobortis. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit.
+                </div>
+            </div>
+            <div className="bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] p-4 m-4">
+                <div className="text-[18px] text-[#889CE7]">Tempus</div>
+                <div className="text-[13px]">
+                Ultrices sagittis orci a scelerisque purus semper eget duis. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Convallis aenean et tortor at risus viverra adipiscing at in.
+                </div>
+            </div>
+            <div className="bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] p-4 m-4">
+                <div className="text-[18px] text-[#889CE7]">Lobortis</div>
+                <div className="text-[13px]">
+                Tortor at auctor urna nunc id. Adipiscing at in tellus integer feugiat scelerisque varius morbi enim. Nec ultrices dui sapien eget mi proin. Quis varius quam quisque id diam. Suspendisse faucibus interdum posuere lorem. Consectetur a erat nam at. Habitant morbi tristique senectus et netus et.
+                </div>
+            </div>
+            <div className="bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] p-4 m-4">
+                <div className="text-[18px] text-[#889CE7]">Egestas</div>
+                <div className="text-[13px]">
+                Egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Imperdiet nulla malesuada pellentesque elit eget gravida. Nisl purus in mollis nunc sed id semper risus. 
+                </div>
+            </div>
+        </div>
+      </div>
+
     </div>
   );
 };
