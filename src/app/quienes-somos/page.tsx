bf7ba866e9bb6b2ba1f762795876952f5b64a5cc
Priset <emanuel.1kll@gmail.com>
2023-12-18 09:48:24 -0400

    Complementando Quienes Somos

--- src/app/quienes-somos/page.tsx
+++ src/app/quienes-somos/page.tsx
@@ -7,18 +7,59 @@ const QuienesSomosPage = () => {
         <div className="quienes-somos-container">
             <section>
                 <h1>¿Quiénes Somos?</h1>
-                <p>Somos un colectivo de emprendedores apasionados por la tecnología, dedicados a impulsar la innovación en el sector. Nuestro equipo, compuesto por expertos en Ingeniería de Sistemas, se especializa en diversas áreas clave, lo que nos permite ofrecer soluciones integrales y vanguardistas. El proyecto que iniciamos es una piedra en la gran pirámide que deseamos construir, debido a que la visión que tenemos no está centrada solamente en el éxito económico, está inclinada hacia la superación individual y colectiva.</p>
+                <p>Somos un colectivo de emprendedores apasionados por la tecnología, 
+                    dedicados a impulsar la innovación en el sector. Nuestro equipo, 
+                    compuesto por expertos en Ingeniería de Sistemas, se especializa 
+                    en diversas áreas clave, lo que nos permite ofrecer soluciones integrales 
+                    y vanguardistas. El proyecto que iniciamos es una piedra en la gran pirámide 
+                    que deseamos construir, debido a que la visión que tenemos no está centrada 
+                    solamente en el éxito económico, está inclinada hacia la superación individual y colectiva.</p>
             </section>
 
             <section>
                 <h2>Misión</h2>
-                <p>Nuestra misión es desentrañar y simplificar los desafíos tecnológicos, ofreciendo soluciones eficaces y personalizadas. Nos enfocamos en minimizar problemas y maximizar eficiencia, asegurando que cada proyecto se realice con agilidad y precisión. Priorizamos la satisfacción total del cliente, garantizando que cada solución no solo cumpla, sino supere sus expectativas.</p>
+                <p>Nuestra misión es desentrañar y simplificar los desafíos tecnológicos, ofreciendo 
+                    soluciones eficaces y personalizadas. Nos enfocamos en minimizar problemas y maximizar 
+                    eficiencia, asegurando que cada proyecto se realice con agilidad y precisión. 
+                    Priorizamos la satisfacción total del cliente, garantizando que cada solución 
+                    no solo cumpla, sino supere sus expectativas.</p>
             </section>
 
             <section>
                 <h2>Visión</h2>
-                <p>En Aurora, nos proyectamos como una empresa líder y referente en el ámbito tecnológico a nivel nacional. Nuestro objetivo es destacarnos por nuestra capacidad de innovar y por la excelencia en la prestación de servicios de comunicación y desarrollo de nuevas tecnologías. Nos comprometemos a ser un pilar en el avance tecnológico, adaptándose y evolucionando constantemente para satisfacer las demandas dinámicas de nuestros clientes.</p>
+                <p>En Aurora, nos proyectamos como una empresa líder y referente en el ámbito tecnológico 
+                    a nivel nacional. Nuestro objetivo es destacarnos por nuestra capacidad de innovar y 
+                    por la excelencia en la prestación de servicios de comunicación y desarrollo de nuevas 
+                    tecnologías. Nos comprometemos a ser un pilar en el avance tecnológico, adaptándose y 
+                    evolucionando constantemente para satisfacer las demandas dinámicas de nuestros clientes.</p>
             </section>
+
+            <section>
+                <h2>Objetivos y Alcances</h2>
+                <h2>Objetivos</h2>
+                <p>Calidad y Eficiencia: Garantizar la más alta calidad y eficiencia en la prestación de servicios técnicos.</p>
+                <p>Innovación Constante: Mantenernos a la vanguardia tecnológica, incorporando constantemente nuevas tendencias y mejoras en nuestra plataforma.</p>
+                <p>Expansión del Mercado: Ampliar nuestra base de usuarios y técnicos, estableciendo Aurora como la plataforma preferida para soluciones técnicas.</p>
+                <p>Satisfacción del Cliente: Lograr una alta tasa de satisfacción del cliente, fomentando relaciones duraderas y confiables.</p>
+            </section>
+
+            <section>
+                <h2>Alcances</h2>
+                <p>Cobertura Nacional: Establecer presencia en todo el territorio nacional, ofreciendo servicios accesibles y de calidad en diferentes regiones.</p>
+                <p>Diversidad de Servicios: Ampliar el rango de servicios ofrecidos, cubriendo una amplia gama de necesidades en software y hardware.</p>
+                <p>Integración Tecnológica: Implementar tecnologías avanzadas para mejorar la experiencia del usuario y la eficiencia operativa de la plataforma.</p>
+            </section>
+
+            <li>
+            <a
+              className="text-blue-400 hover:font-semibold hover:text-blue-800"
+              href="https://docs.google.com/document/d/1E94IZehhOqlgI20UJkv6xbMEwNzj_Z9rRlC5ddWvqRc/edit?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Fase de Presentación
+            </a>
+            </li>
         </div>
     );
 };
