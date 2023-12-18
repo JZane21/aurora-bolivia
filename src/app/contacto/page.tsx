74055d4bdafee55a60b22c07540bef927e952a54
Amy312 <katsukiamy1.gmail.com>
2023-12-18 09:56:28 -0400

    working

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,6 +1,45 @@
 const ContactoPage = () => {
-    return(
-        <>Contacto uwu</>
-    )
-}
+  return (
+    <>
+      <div className="flex flex-row w-full items-center justify-center">
+        <div className="flex flex-col  bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] shadow-md text-center items-center w-1/6 p-4 m-4">
+          <label className="text-[#889CE7] font-semibold text-2xl my-6">
+            Natalia Bilbao
+          </label>
+          <label className="text-white font-light">+591 61143115</label>
+        </div>
+        <div className="flex flex-col  bg-slate-100 rounded-3xl border-8 shadow-md text-center items-center w-1/6 p-4 m-4">
+          <label className="text-[#889CE7] font-semibold text-2xl my-6">
+            Ander Callyan
+          </label>
+          <label className="text-white font-light">+591 62449894 </label>
+        </div>
+        <div className="flex flex-col  bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] shadow-md text-center items-center w-1/6 p-4 m-4">
+          <label className="text-[#889CE7] font-semibold text-2xl my-6">
+            Jose Carrasco
+          </label>
+          <label className="text-white font-light">591 67004903</label>
+        </div>
+        <div className="flex flex-col  bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] shadow-md text-center items-center w-1/6 p-4 m-4">
+          <label className="text-[#889CE7] font-semibold text-2xl my-6">
+            Ignacio Lizarazu
+          </label>
+          <label className="text-white font-light">+591 68150005</label>
+        </div>
+        <div className="flex flex-col  bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] shadow-md text-center items-center w-1/6 p-4 m-4">
+          <label className="text-[#889CE7] font-semibold text-2xl my-6">
+            Emanuel Lozano
+          </label>
+          <label className="text-white font-light">+ 591 76710956</label>
+        </div>
+        <div className="flex flex-col  bg-[#1B1C1E] rounded-3xl border-8 border-[#121213] shadow-md text-center items-center w-1/6 p-4 m-4">
+          <label className="text-[#889CE7] font-semibold text-2xl my-6">
+            Ambar Rojas
+          </label>
+          <label className="text-white font-light">+591 67067311</label>
+        </div>
+      </div>
+    </>
+  );
+};
 export default ContactoPage;
